01dcfe40c74ea3573f1c7d2825671d0e067857db
Amena Khan <amenaikhn@yahoo.com>
2020-09-28 12:57:56 -0400

    Super Balanced Tree

---
+++ interviewcake/src/interviewcake/balancedTree.java
@@ -0,0 +1,160 @@
+package interviewcake;
+
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+
+import static org.junit.Assert.*;
+
+public class balancedTree {
+
+    public static class BinaryTreeNode {
+
+        public int value;
+        public BinaryTreeNode left;
+        public BinaryTreeNode right;
+
+        public BinaryTreeNode(int value) {
+            this.value = value;
+        }
+
+        public BinaryTreeNode insertLeft(int leftValue) {
+            this.left = new BinaryTreeNode(leftValue);
+            return this.left;
+        }
+
+        public BinaryTreeNode insertRight(int rightValue) {
+            this.right = new BinaryTreeNode(rightValue);
+            return this.right;
+        }
+    }
+
+    public static boolean isBalanced(BinaryTreeNode treeRoot) {
+
+        
+        
+
+        return false;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // tests
+
+    @Test
+    public void fullTreeTest() {
+        final BinaryTreeNode root = new BinaryTreeNode(5);
+        final BinaryTreeNode a = root.insertLeft(8);
+        final BinaryTreeNode b = root.insertRight(6);
+        a.insertLeft(1);
+        a.insertRight(2);
+        b.insertLeft(3);
+        b.insertRight(4);
+        final boolean result = isBalanced(root);
+        assertTrue(result);
+    }
+
+    @Test
+    public void bothLeavesAtTheSameDepthTest() {
+        final BinaryTreeNode root = new BinaryTreeNode(3);
+        root.insertLeft(4).insertLeft(1);
+        root.insertRight(2).insertRight(9);
+        final boolean result = isBalanced(root);
+        assertTrue(result);
+    }
+
+    @Test
+    public void leafHeightsDifferByOneTest() {
+        final BinaryTreeNode root = new BinaryTreeNode(6);
+        root.insertLeft(1);
+        root.insertRight(0).insertRight(7);
+        final boolean result = isBalanced(root);
+        assertTrue(result);
+    }
+
+    @Test
+    public void leafHeightsDifferByTwoTest() {
+        final BinaryTreeNode root = new BinaryTreeNode(6);
+        root.insertLeft(1);
+        root.insertRight(0).insertRight(7).insertRight(8);
+        final boolean result = isBalanced(root);
+        assertFalse(result);
+    }
+
+    @Test
+    public void bothSubTreesSuperbalancedTest() {
+        final BinaryTreeNode root = new BinaryTreeNode(1);
+        root.insertLeft(5);
+        final BinaryTreeNode b = root.insertRight(9);
+        b.insertLeft(8).insertLeft(7);
+        b.insertRight(5);
+        final boolean result = isBalanced(root);
+        assertFalse(result);
+    }
+
+    @Test
+    public void bothSubTreesSuperbalancedTwoTest() {
+        final BinaryTreeNode root = new BinaryTreeNode(1);
+        final BinaryTreeNode a = root.insertLeft(2);
+        a.insertLeft(3);
+        a.insertRight(7).insertRight(8);
+        root.insertRight(4).insertRight(5).insertRight(6).insertRight(9);
+        final boolean result = isBalanced(root);
+        assertFalse(result);
+    }
+
+    @Test
+    public void threeLeavesAtDifferentLevelsTest() {
+        final BinaryTreeNode root = new BinaryTreeNode(1);
+        final BinaryTreeNode a = root.insertLeft(2);
+        final BinaryTreeNode b = a.insertLeft(3);
+        a.insertRight(4);
+        b.insertLeft(5);
+        b.insertRight(6);
+        root.insertRight(7).insertRight(8).insertRight(9).insertRight(10);
+        final boolean result = isBalanced(root);
+        assertFalse(result);          
+    }
+
+    @Test
+    public void onlyOneNodeTest() {
+        final BinaryTreeNode root = new BinaryTreeNode(1);
+        final boolean result = isBalanced(root);
+        assertTrue(result);
+    }
+
+    @Test
+    public void treeIsLinkedListTest() {
+        final BinaryTreeNode root = new BinaryTreeNode(1);
+        root.insertRight(2).insertRight(3).insertRight(4);
+        final boolean result = isBalanced(root);
+        assertTrue(result);
+    }
+
+    public static void main(String[] args) {
+        Result result = JUnitCore.runClasses(balancedTree.class);
+        for (Failure failure : result.getFailures()) {
+            System.out.println(failure.toString());
+        }
+        if (result.wasSuccessful()) {
+            System.out.println("All tests passed.");
+        }
+    }
+}
