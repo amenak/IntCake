fbf9f45c55ce56d7cbe00ab8a52a8f4eccdd58af
Amena Khan <amenaikhn@yahoo.com>
2020-12-04 07:47:49 -0500

    Second largest BST first attempt no solution

---
+++ interviewcake/src/interviewcake/secondLargestBST.java
@@ -0,0 +1,185 @@
+package interviewcake;
+
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+import static org.junit.Assert.*;
+
+public class secondLargestBST {
+
+    public static class BinaryTreeNode {
+
+        public int value;
+        public BinaryTreeNode left;
+        public BinaryTreeNode right;
+
+        public BinaryTreeNode(int value) {
+            this.value = value;
+        }
+
+        public BinaryTreeNode insertLeft(int leftValue) {
+            this.left = new BinaryTreeNode(leftValue);
+            return this.left;
+        }
+
+        public BinaryTreeNode insertRight(int rightValue) {
+            this.right = new BinaryTreeNode(rightValue);
+            return this.right;
+        }
+    }
+
+    public static int findSecondLargest(BinaryTreeNode rootNode) {
+
+        // find the second largest item in the binary search tree
+    	if(rootNode == null) throw new IllegalArgumentException ("empty tree"); //it should be an exception for an empty tree
+    	if(rootNode.right == null && rootNode.left == null ) throw new IllegalArgumentException("no second node in tree"); //why cant we just return the root? OH because its not the second largest
+    	    	
+    	int max = -1; //idk if this should be -1
+    	
+    	if(rootNode.right != null) {
+    		max = secondMaxRight(rootNode.right, rootNode.value);
+    	} else if(rootNode.left != null){
+    		max = secondMaxLeft(rootNode.left); //this is where my code is failing. its only checking the left subtree for the root. it needs 
+    		//to check it for right most node as well or well ALL nodes. 
+    	}
+    	
+        return max;
+    }
+
+    public static int secondMaxRight(BinaryTreeNode node, int parentVal) {
+    	if(node == null) return -1;
+    	
+    	int max = secondMaxRight(node.right, node.value);
+    	
+    	//is current node leaf node? return parent val
+    	if(max == -1) {
+    		return parentVal; //will not work for left subtree, you need to return the current nodes value 
+    	}
+    	
+    	return max; //not sure if i should return the current nodes value. nope. it should return what the child returns. 
+    	
+    }
+    
+    public static int secondMaxLeft(BinaryTreeNode node) {
+    	if(node == null) return -1; 
+    	
+    	int max = secondMaxLeft(node.right);
+    	
+    	if(max == -1)
+    		max = node.value; 
+    	
+    	return max;
+    }
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // tests
+
+    @Test
+    public void findSecondLargestTest() {
+        final BinaryTreeNode root = new BinaryTreeNode(50);
+        final BinaryTreeNode a = root.insertLeft(30);
+        a.insertLeft(10);
+        a.insertRight(40);
+        final BinaryTreeNode b = root.insertRight(70);
+        b.insertLeft(60);
+        b.insertRight(80);
+        final int actual = findSecondLargest(root);
+        final int expected = 70;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void largestHasLeftChildTest() {
+        final BinaryTreeNode root = new BinaryTreeNode(50);
+        final BinaryTreeNode a = root.insertLeft(30);
+        a.insertLeft(10);
+        a.insertRight(40);
+        root.insertRight(70).insertLeft(60);
+        final int actual = findSecondLargest(root);
+        final int expected = 60;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void largestHasLeftSubtreeTest() {
+        final BinaryTreeNode root = new BinaryTreeNode(50);
+        final BinaryTreeNode a = root.insertLeft(30);
+        a.insertLeft(10);
+        a.insertRight(40);
+        final BinaryTreeNode b = root.insertRight(70).insertLeft(60);
+        b.insertLeft(55).insertRight(58);
+        b.insertRight(65);
+        final int actual = findSecondLargest(root);
+        final int expected = 65;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void secondLargestIsRootNodeTest() {
+        final BinaryTreeNode root = new BinaryTreeNode(50);
+        final BinaryTreeNode a = root.insertLeft(30);
+        a.insertLeft(10);
+        a.insertRight(40);
+        root.insertRight(70);
+        final int actual = findSecondLargest(root);
+        final int expected = 50;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void descendingLinkedListTest() {
+        final BinaryTreeNode root = new BinaryTreeNode(50);
+        root.insertLeft(40).insertLeft(30).insertLeft(20);
+        final int actual = findSecondLargest(root);
+        final int expected = 40;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void ascendingLinkedListTest() {
+        final BinaryTreeNode root = new BinaryTreeNode(50);
+        root.insertRight(60).insertRight(70).insertRight(80);
+        final int actual = findSecondLargest(root);
+        final int expected = 70;
+        assertEquals(expected, actual);
+    }
+
+    @Test(expected = Exception.class)
+    public void exceptionWithTreeThatHasOneNodeTest() {
+        final BinaryTreeNode root = new BinaryTreeNode(50);
+        findSecondLargest(root);
+    }
+
+    @Test(expected = Exception.class)
+    public void exceptionWithEmptyTreeTest() {
+        findSecondLargest(null);
+    }
+
+    public static void main(String[] args) {
+        Result result = JUnitCore.runClasses(secondLargestBST.class);
+        for (Failure failure : result.getFailures()) {
+            System.out.println(failure.toString());
+        }
+        if (result.wasSuccessful()) {
+            System.out.println("All tests passed.");
+        }
+    }
+}
