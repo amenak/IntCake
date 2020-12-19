8cbb243d3c6fffca04bed7a46c5fb2416ea2bf92
Amena Khan <amenaikhn@yahoo.com>
2020-12-19 10:57:14 -0500

    MeshMessage review and notes

--- interviewcake/src/interviewcake/TreesAndGraphsReview/MeshMessage121820.java
+++ interviewcake/src/interviewcake/TreesAndGraphsReview/MeshMessage121820.java
@@ -13,7 +13,7 @@ import static org.junit.Assert.*;
 
 public class MeshMessage121820 {
 	
-	public String [] constructPath(String startNode, String endNode, HashMap<String,String> nodesMap) {
+	public static String [] constructPath(String startNode, String endNode, Map<String,String> nodesMap) {
 		
 		List<String> path = new ArrayList<String>();
 		
@@ -26,6 +26,7 @@ public class MeshMessage121820 {
 		Collections.reverse(path);
 		return path.toArray(new String [path.size()]); 
 	}
+	
     public static String[] getPath(Map<String, String[]> graph, String startNode, String endNode) {
     	if(startNode.equals(endNode)) return new String [] {startNode};
     	if(!graph.containsKey(endNode)) throw new IllegalArgumentException("end node does not exist");
@@ -36,39 +37,40 @@ public class MeshMessage121820 {
         q.add(startNode);
         nodeMap.put(startNode, null);
         
+       int iteration = 0;
         while(!q.isEmpty()) {
+        	iteration = iteration + 1;
         	String node = q.remove(); //could i pop from deque but not from queue? cool
         	
-        	if(node.equals(endNode)) { //more iterations no? if you put it here instead of the loop. YES if u put it in for loop its less iterations.
+        	/*if(node.equals(endNode)) { //more iterations no? if you put it here instead of the loop. YES if u put it in for loop its less iterations.
         		//basically you have to wait until you reach adam to break. if in the for loop  you can break when u reach amelia. 
-        			break; // breaks for loop or while loop?
-        	}
+        			//break; // breaks for loop or while loop?
+        		return constructPath(startNode, endNode, nodeMap); 
+        	}*/
+        	
+
         	
         	for(String neighbor : graph.get(node)) {
         		if(!nodeMap.containsKey(neighbor)) {
         			q.add(neighbor);
         			nodeMap.put(neighbor, node);
         		}
+        		
+        	if(node.equals(endNode)) { //more iterations no? if you put it here instead of the loop. YES if u put it in for loop its less iterations.
+        		//basically you have to wait until you reach adam to break. if in the for loop  you can break when u reach amelia. 
+        			//break; // breaks for loop or while loop?
+        		
+        		return constructPath(startNode, endNode, nodeMap); 
         	}
+        		
+        		
+        	}
+        	
         	
         }
         
-        //create the path
-        List<String> path = new ArrayList<String>();
-        path.add(endNode);
-        String current = nodeMap.get(endNode);
-        
-        while(current != null){
-        	path.add(current);
-        	current = nodeMap.get(current);
-        }
-        
-        if(path.size() == 1) { //i have to do this because there code calls construct path. which is admittedly more cleaner. 
-        	return null;
-        }
-        
-        Collections.reverse(path);
-        return path.toArray(new String [path.size()]);
+        return null;
+
     }
 
 
