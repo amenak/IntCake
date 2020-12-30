7b77e625927ffff48e1f65a46ec48ce68070e34b
Amena Khan <amenaikhn@yahoo.com>
2020-12-30 09:46:18 -0500

    RecursiveStringPermutation test passed with notes

--- interviewcake/src/interviewcake/DynamicProgrammingRecursion/RecursiveStringPermutation.java
+++ interviewcake/src/interviewcake/DynamicProgrammingRecursion/RecursiveStringPermutation.java
@@ -14,38 +14,39 @@ import static org.junit.Assert.*;
 public class RecursiveStringPermutation {
 
     public static Set<String> getPermutations(String inputString) {
-    	//if(inputString.isEmpty()) return new HashSet<String>();
+    	Set<String> newSet = new HashSet<>();
     	
-    	Set<String> newSet = new HashSet<String>();
-    	if(inputString.length() == 1) {
+    	if(inputString.length() <= 1) { //if empty string with 0 length, you need to still return a set with an empty string. 
     		newSet.add(inputString);
     	}
     	
     	if(inputString.length() >= 2) {
-	    	Set<String> prevSet = getPermutations(inputString.substring(1)); //cut off first character //will it pass in empty string??
+    		//cut off first character //will it pass in empty string??
+	    	Set<String> prevSet = getPermutations(inputString.substring(1)); //O(N) -2 ish
 	    	char beginingChar = inputString.charAt(0);
 	    	
-	    	for(String s : prevSet) {
+	    	for(String s : prevSet) { //O(N!)
 	    		String word = beginingChar + s; //can i add a char like this to a string
 	    		newSet.add(word);
 	    		
 	    		char [] wordArr = word.toCharArray();
-	    		for(int i=0; i < wordArr.length-1; i++) {
+	    		for(int i=0; i < wordArr.length-1; i++) { //as long as the word is o(n)
 	    			char temp = wordArr[i+1];
 	    			wordArr[i+1] = wordArr[i];
 	    			wordArr[i] = temp;
 	    			//how do i change the character at specific index in a string - i converted it to a character array
-	    			newSet.add(wordArr.toString());
+	    			String swaped = new String (wordArr); //you can not convert a char array to a string by doing .toString();
+	    			newSet.add(swaped);
 	    		}
 	    	}
     	}
-      
-         
+
         return newSet;
     }
 
 
-
+//Complexity: O(n^2 * n!) ? not sure. it is O(N!) explained in notes
+//Space: O(n)
 
 
 
