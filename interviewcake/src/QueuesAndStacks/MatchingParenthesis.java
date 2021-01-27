0b1a8673c5df95627eb31d3fdfafffd2b80f11ce
Amena Khan <amenaikhn@yahoo.com>
2021-01-27 08:20:46 -0500

    MatchingParenthesis my solution

--- interviewcake/src/QueuesAndStacks/MatchingParenthesis.java
+++ interviewcake/src/QueuesAndStacks/MatchingParenthesis.java
@@ -10,11 +10,34 @@ import static org.junit.Assert.*;
 public class MatchingParenthesis {
 
     public static int getClosingParen(String sentence, int openingParenIndex) {
-
+    	
+    	
         // find the position of the matching closing parenthesis
+    	
+    	//String tracker = "";
+    	StringBuilder parenthesis = new StringBuilder();
+    	
+        for(int i=openingParenIndex; i <sentence.length(); i++) {
+        	if(sentence.charAt(i) == '(') {
+        		parenthesis.append("("); //string builder amortizes to O(1)
+        		//tracker += "("; can take up to n^2
+        	}
+        	if(sentence.charAt(i) == ')' && parenthesis.length() != 0) {
+        		//String chopped = parenthesis.substring(0, parenthesis.length()-1); //how do you replace the current string builder..
+        		parenthesis.deleteCharAt(parenthesis.length()-1);
+        		//tracker = tracker.substring(0, tracker.length()-1);
+        	}
+        	
+        	if(parenthesis.length() == 0) {
+        		return i;
+        	}
+        }
         
+        if(parenthesis.length() != 0)
+        	throw new IllegalArgumentException("Parenthesis has no closer");
+        
+        return -1;
 
-        return 0;
     }
 
 
