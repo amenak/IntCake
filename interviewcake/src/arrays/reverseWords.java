6ddd171eacbf37c6d84b942bbc2c300cc3f5637a
Amena Khan <amenaikhn@yahoo.com>
2021-02-14 11:16:11 -0500

    Reverse Words my solution

--- interviewcake/src/arrays/reverseWords.java
+++ interviewcake/src/arrays/reverseWords.java
@@ -9,16 +9,50 @@ import static org.junit.Assert.*;
 
 public class reverseWords {
 
-    public static void reverseWords(char[] message) {
-
-        // decode the message by reversing the words
+    public static void reverseWords1(char[] message) {
+    	if(message.length == 0 ) return;
+    	
+    	//reverse entire array
+    	revWordsHelper(0, message.length-1, message);
+    	
+    	//reverse every word
+    	int wordStartIdx = 0;
+        for(int i=0; i < message.length; i++) {
+        	if(message[i] == ' ') {
+        		revWordsHelper(wordStartIdx, i-1, message);
+        		
+        		
+        			wordStartIdx = i+1;
+        		
+        	}
+        	
+        	//misses last word 
+        	if(i == message.length-1) {
+        		revWordsHelper(wordStartIdx, i, message); //should also reverse back a single word
+        	}
+        	
+        }
+        
         
 
+    }
+    
+    //is the array pass by reference or value?
+    public static void revWordsHelper(int startIdx, int endIdx, char [] msg) {
+    	if ((endIdx-startIdx) < 1) return;
+    	
+    	int mid = startIdx + (endIdx-startIdx)/2; //mistake here
+    	
+    	for(int i=startIdx; i <= mid; i++) {
+    		char temp = msg[i];
+    		msg[i] = msg[endIdx-(i-startIdx)]; //mistake here
+    		msg[endIdx-(i-startIdx)] = temp;
+    	}
     }
 
 
 
-
+//Time O(N)
 
 
 
@@ -39,7 +73,7 @@ public class reverseWords {
     public void oneWordTest() {
         final char[] expected = "vault".toCharArray();
         final char[] actual = "vault".toCharArray();
-        reverseWords(actual);
+        reverseWords1(actual);
         assertArrayEquals(expected, actual);
     }
 
@@ -47,7 +81,7 @@ public class reverseWords {
     public void twoWordsTest() {
         final char[] expected = "cake thief".toCharArray();
         final char[] actual = "thief cake".toCharArray();
-        reverseWords(actual);
+        reverseWords1(actual);
         assertArrayEquals(expected, actual);
     }
 
@@ -55,7 +89,7 @@ public class reverseWords {
     public void threeWordsTest() {
         final char[] expected = "get another one".toCharArray();
         final char[] actual = "one another get".toCharArray();
-        reverseWords(actual);
+        reverseWords1(actual);
         assertArrayEquals(expected, actual);
     }
 
@@ -63,7 +97,7 @@ public class reverseWords {
     public void multipleWordsSameLengthTest() {
         final char[] expected = "the cat ate the rat".toCharArray();
         final char[] actual = "rat the ate cat the".toCharArray();
-        reverseWords(actual);
+        reverseWords1(actual);
         assertArrayEquals(expected, actual);
     }
 
@@ -71,7 +105,7 @@ public class reverseWords {
     public void multipleWordsDifferentLengthsTest() {
         final char[] expected = "chocolate bundt cake is yummy".toCharArray();
         final char[] actual = "yummy is cake bundt chocolate".toCharArray();
-        reverseWords(actual);
+        reverseWords1(actual);
         assertArrayEquals(expected, actual);
     }
 
@@ -79,7 +113,7 @@ public class reverseWords {
     public void emptyStringTest() {
         final char[] expected = "".toCharArray();
         final char[] actual = "".toCharArray();
-        reverseWords(actual);
+        reverseWords1(actual);
         assertArrayEquals(expected, actual);
     }
 
